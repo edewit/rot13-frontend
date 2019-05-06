c61927690dda1963c1194bb2bc452afdfb820b63
Andrew Lee Rubinger <alr@alrubinger.com>
2019-05-06 03:07:26 -0400

    Simple working frontend to call ROT13 backend

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+  
+  constructor(){
+    super();
+
+    this.state = {
+      cleartext: '',
+      rot13: ''
+    }
+
+    this.rot13 = this.rot13.bind(this);
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <textarea value={this.state.cleartext} onChange={this.rot13}></textarea>
+        <br />ROT13: {this.state.rot13}
       </div>
     );
   }
+
+  rot13(event) {
+
+    const url = "http://rot13-rot13-backend.7e14.starter-us-west-2.openshiftapps.com/api/greeting?name=";
+    const input = event.target.value;
+    this.setState({
+      cleartext: input
+    });
+    
+    axios.get(url + input).then(response => this.setState({
+      rot13: response.data.content
+    }));
+    
+  };
 }
 
 export default App;
